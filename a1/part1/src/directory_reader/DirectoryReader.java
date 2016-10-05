f3bd92834f8aa54d214f401397b775842f5ac317
sendorek <d.sendorek@mail.utoronto.ca>
2016-10-05 17:04:46 -0400

    Removed unnecessary for loop

--- a1/part1/src/directory_reader/DirectoryReader.java
+++ a1/part1/src/directory_reader/DirectoryReader.java
@@ -31,13 +31,11 @@ public class DirectoryReader {
 			for (int i = 0; i < files.length; i++) {
 				if (files[i].isDirectory()) {
 					System.out.println("- " + files[i].getName() + "/");
-				}
-			}
-			for (int i = 0; i < files.length; i++) {
-				if (files[i].isFile()) {
+				} else {
 					System.out.println("- " + files[i].getName());
 				}
 			}
+			
 		}
 	}
 }

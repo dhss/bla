fef5d65254ac6463ac7db3d7976807ee0197fa00
sendorek <d.sendorek@mail.utoronto.ca>
2016-10-05 17:28:07 -0400

    Implemented Author class

--- lab3/src/week3lab/Author.java
+++ lab3/src/week3lab/Author.java
@@ -2,9 +2,39 @@ package week3lab;
 
 public class Author {
 
+	private String firstName;
+	private String lastName;
+	
+	public Author(String first, String last) {
+		this.firstName = first;
+		this.lastName = last;
+	}
+	
+	private String getFirstName() {
+		return this.firstName;
+	}
+	
+	private void setFirstName(String first) {
+		this.firstName = first;
+	}
+	
+	private String getLastName() {
+		return this.lastName;
+	}
+	
+	private void setLastName(String last) {
+		this.lastName = last;
+	}
+	
+	public String toString() {
+		return this.lastName + ", " + this.firstName;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		
 	}
 
 }

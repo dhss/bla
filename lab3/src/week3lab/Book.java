978ddfdf8e83cf61ec1f19ec960c1402d1464e3d
sendorek <d.sendorek@mail.utoronto.ca>
2016-10-05 17:15:39 -0400

    Setup for lab 3

---
+++ lab3/src/week3lab/Book.java
@@ -0,0 +1,10 @@
+package week3lab;
+
+public class Book {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ lab3/src/week3lab/Store.java
@@ -0,0 +1,10 @@
+package week3lab;
+
+public class Store {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
